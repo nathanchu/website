af14fbbe5a34175786ad0edbb8f7782a3e7b68d2
Nathan Chu <63111210+nathanchu@users.noreply.github.com>
2020-12-15 19:58:05 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,10 @@ module.exports = {
         background_color: '#ffffff',
         theme_color: '#4169e1',
         display: 'minimal-ui',
-        icon: './src/images/codebackground.svg'
+        icon: './src/images/codebackground.svg',
+        icon_options: {
+          purpose: 'any maskable'
+        }
       }
     },
     'gatsby-plugin-mdx',
