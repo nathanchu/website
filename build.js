58d19bdd45b165c9786577e01fd984dd54a17432
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-12-20 13:55:40 -0500

    Change puppeteer to playwright

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
-const puppeteer = require('puppeteer')
+const playwright = require('playwright')
 const path = require('path')
-const fs = require('fs');
+const fs = require('fs')
 
-function newWriteFile(screenshot) {
+function WriteFile(screenshot) {
   return function writeFile(file) {
     return new Promise((resolve, reject) => {
       fs.writeFile(path.join(__dirname, file), screenshot, (err) => {
@@ -13,18 +13,23 @@ function newWriteFile(screenshot) {
   }
 }
 
-;(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 900 })
-  async function screenshot(site, file) {
+const main = async (obj) => {
+  const browser = await playwright['chromium'].launch();
+  const context = await browser.newContext();
+  const screenshot = async (site, file) => {
+    const page = await context.newPage();
+    await page.setViewportSize({ width: 1200, height: 900 })
     await page.goto(site);
-    const writeFile = newWriteFile(await page.screenshot())
+    const writeFile = WriteFile(await page.screenshot())
     if (Array.isArray(file)) await Promise.all(file.map(writeFile));
     if (typeof file === 'string') await writeFile(file)
     return file
   }
-  await screenshot('https://nathanchu.com/', ['static/banner.png', 'src/images/projects/website.png'])
-  await screenshot('https://weather.rayyansaidi.com/', 'src/images/projects/weather.png')
+  await Promise.all(Object.entries(obj).map(e => screenshot(...e)))
   await browser.close();
-})()
+}
+
+main({
+  'https://nathanchu.com/': ['static/banner.png', 'src/images/projects/website.png'],
+  'https://weather.rayyansaidi.com/': 'src/images/projects/weather.png'
+})
