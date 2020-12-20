383b94b696c92b4de24e37ef7e25419e23a7a216
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-12-20 14:10:40 -0500

    Run prettier, work on build

--- build.js
+++ build.js
@@ -1,35 +1,46 @@
-const playwright = require('playwright')
+const { chromium } = require('playwright')
 const path = require('path')
 const fs = require('fs')
 
-function WriteFile(screenshot) {
-  return function writeFile(file) {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(path.join(__dirname, file), screenshot, (err) => {
-        if (err) return reject(err)
-        resolve()
-      })
+const WriteFile = screenshot => file =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(path.join(__dirname, file), screenshot, err => {
+      if (err) return reject(err)
+      resolve()
     })
-  }
-}
+  })
 
-const main = async (obj) => {
-  const browser = await playwright['chromium'].launch();
-  const context = await browser.newContext();
-  const screenshot = async (site, file) => {
-    const page = await context.newPage();
-    await page.setViewportSize({ width: 1200, height: 900 })
-    await page.goto(site);
-    const writeFile = WriteFile(await page.screenshot())
-    if (Array.isArray(file)) await Promise.all(file.map(writeFile));
-    if (typeof file === 'string') await writeFile(file)
-    return file
+const main = async obj => {
+  try {
+    const browser = await chromium.launch()
+    const context = await browser.newContext()
+    const screenshot = async (site, file) => {
+      try {
+        const page = await context.newPage()
+        await page.setViewportSize({ width: 1200, height: 900 })
+        await page.goto(site)
+        const writeFile = WriteFile(await page.screenshot())
+        if (Array.isArray(file)) await Promise.all(file.map(writeFile))
+        if (typeof file === 'string') await writeFile(file)
+        return file
+      } catch (e) {
+        console.error(e)
+        return
+      }
+    }
+    await Promise.all(Object.entries(obj).map(e => screenshot(...e)))
+    await browser.close()
+    return
+  } catch (e) {
+    console.error(e)
+    return
   }
-  await Promise.all(Object.entries(obj).map(e => screenshot(...e)))
-  await browser.close();
 }
 
 main({
-  'https://nathanchu.com/': ['static/banner.png', 'src/images/projects/website.png'],
+  'https://nathanchu.com/': [
+    'static/banner.png',
+    'src/images/projects/website.png'
+  ],
   'https://weather.rayyansaidi.com/': 'src/images/projects/weather.png'
-})
+}).catch(e => console.error(e))
--- src/components/projects.js
+++ src/components/projects.js
@@ -9,14 +9,16 @@ const projects = [
     title: 'My Website',
     website: 'https://nathanchu.com/',
     repo: 'nathanchu/website',
-    description: "The website you're looking at right now! It's built using Gatsby, and hosted on Vercel. It's highly customizable if you want to do that, too!",
+    description:
+      "The website you're looking at right now! It's built using Gatsby, and hosted on Vercel. It's highly customizable if you want to do that, too!",
     image: <StaticImage src="../images/projects/website.png" />
   },
   {
     title: 'Weather App',
     repo: 'Ryyn-Sd/weather',
     website: 'https://weather.rayyansaidi.com/',
-    description: "A weather app I worked on using openweathermap and React. It detects your location through your ip, but also allows you to change your zip code.",
+    description:
+      'A weather app I worked on using openweathermap and React. It detects your location through your ip, but also allows you to change your zip code.',
     image: <StaticImage src="../images/projects/weather.png" />
   }
 ]
@@ -25,18 +27,29 @@ const Projects = () => {
   return (
     <div className={styles.projects}>
       <h1>Projects</h1>
-      {
-        projects.map(({title, repo, website, description, image}) => (
-          <div className={styles.project}>
-            <Card>
-              <h2><a {...aProps} href={website}>{title}</a> (<a {...aProps} href={`https://github.com/${repo}`}>{repo}</a>)</h2>
-              <br />
-              <p>{description}</p>
-            </Card>
-            <ImageCard><a {...aProps} href={website}>{image}</a></ImageCard>
-          </div>
-        ))
-      }
+      {projects.map(({ title, repo, website, description, image }) => (
+        <div className={styles.project}>
+          <Card>
+            <h2>
+              <a {...aProps} href={website}>
+                {title}
+              </a>{' '}
+              (
+              <a {...aProps} href={`https://github.com/${repo}`}>
+                {repo}
+              </a>
+              )
+            </h2>
+            <br />
+            <p>{description}</p>
+          </Card>
+          <ImageCard>
+            <a {...aProps} href={website}>
+              {image}
+            </a>
+          </ImageCard>
+        </div>
+      ))}
     </div>
   )
 }
