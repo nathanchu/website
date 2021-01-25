b812aa58f8497f3d91ae16ea4099ecebdb4cc7cc
Nathan Chu <63111210+nathanchu@users.noreply.github.com>
2021-01-24 21:28:11 -0500

    Re-add matomo

--- src/html.js
+++ src/html.js
@@ -12,15 +12,15 @@ export default function HTML (props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        {/* <link rel="dns-prefetch" href="//analytics.nathanchu.com/" /> */}
+        <link rel="dns-prefetch" href="//analytics.nathanchu.com/" />
         {/* Matomo */}
-        {/* <script
+        <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html:
               '(function(){"use strict";var _paq=window._paq=window._paq||[];_paq.push(["trackPageView"]),_paq.push(["enableLinkTracking"]),function(){var a="//analytics.nathanchu.com/";_paq.push(["setTrackerUrl",a+"matomo.php"]),_paq.push(["setSiteId","1"]);var e=(t=document).createElement("script"),t=t.getElementsByTagName("script")[0];e.type="text/javascript",e.async=!0,e.src=a+"matomo.js",t.parentNode.insertBefore(e,t)}();})();'
           }}
-        /> */}
+        />
         {/* End Matomo Code */}
       </head>
       <body {...props.bodyAttributes}>
